262135200b486ff458a513ac2797a756bc7846eb
Cezar Leigos <cezarleigos28@outlook.com>
2021-01-18 12:41:18 -0300

    Função com ou sem HTML
    Dessa vez nós adicionamos uma função para que o usuário possa escolher se ele deseja gerar um texto com html ou apenas um texto cumum.

--- app/js/base.js
+++ app/js/base.js
@@ -2,61 +2,78 @@
 
 /*
    Lista de palavras aleatórias para gerar o resultado final
-*/
-var listadepalavras = [
-'a','muda', 'canni','herem','ju','teret','pale','alt','shift','hum','poo',
-'durt','homer','nii','casur','thir','kak','vert','bug','lag','leig',
-'yol','peli','datt','woldeeemm','totonar','verick','trut','qqqe','zim','rat',
-'kadi','yuta','susunaru','toktikpak','beru-cara','borg','rua','il','el','nome',
-'cometo','paru','willcom','grumpjuir','calcado','risi','batabata','safirm','menorenpo','aar',
-'seghyh','wejkt','borgmand','yyum','kadiho','noverkan','monic','vumm','zerimbarner','oioi',
-'miriade', 'kava', 'toremureno', 'rorodem', 'turing', 'durmir', 'queen', 'roder', 'mi', 'juana',
-'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-];
+   */
+   var listadepalavras = [
+   'a','muda', 'canni','herem','ju','teret','pale','alt','shift','hum','poo',
+   'durt','homer','nii','casur','thir','kak','vert','bug','lag','leig',
+   'yol','peli','datt','woldeeemm','totonar','verick','trut','qqqe','zim','rat',
+   'kadi','yuta','susunaru','toktikpak','beru-cara','borg','rua','il','el','nome',
+   'cometo','paru','willcom','grumpjuir','calcado','risi','batabata','safirm','menorenpo','aar',
+   'seghyh','wejkt','borgmand','yyum','kadiho','noverkan','monic','vumm','zerimbarner','oioi',
+   'miriade', 'kava', 'toremureno', 'rorodem', 'turing', 'durmir', 'queen', 'roder', 'mi', 'juana',
+   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+   ];
 
 /*
    Essa função é usada para gerar aleatoriamente uma palavra da lista de palavras
-*/
-function getRandomInt(min, max) {
-   min = Math.ceil(min);
-   max = Math.floor(max);
-   var resultadogerador =  Math.floor(Math.random() * (max - min)) + min;
-   return listadepalavras[resultadogerador];
-}
+   */
+   function getRandomInt(min, max) {
+     min = Math.ceil(min);
+     max = Math.floor(max);
+     var resultadogerador =  Math.floor(Math.random() * (max - min)) + min;
+     return listadepalavras[resultadogerador];
+   }
 
-document.getElementById("botaogerartexto").addEventListener("click", function() {
+   document.getElementById("botaogerartexto").addEventListener("click", function() {
 
-   var textopronto = "";
-   var inicioparagrafo = 0;
-   var fimparagrafo = document.getElementById("npalavras").value;
-   var numeroparagrafos = document.getElementById("nparagrafos").value;
+     var textopronto = "";
+     var inicioparagrafo = 0;
+     var fimparagrafo = document.getElementById("npalavras").value;
+     var numeroparagrafos = document.getElementById("nparagrafos").value;
 
-   for(var j = 0; j < numeroparagrafos; j++){
+     var radiotcomum = document.getElementById("radiotcomum").checked ;
+     var radiothtml = document.getElementById("radiothtml").checked ;
 
-     for(var i = inicioparagrafo; i < fimparagrafo; i++){
-         
-        if(i == 0){
+     
 
-           textopronto += getRandomInt(0,listadepalavras.length).charAt(0).toUpperCase() + 
-           getRandomInt(1,listadepalavras.length).slice(1);
+     for(var j = 0; j < numeroparagrafos; j++){
+       
+        
+        if(radiothtml){
+           textopronto += "&#60;&#112&#62;" + "<br>";
+        }
 
-        }else{
 
-           textopronto += " " + getRandomInt(1,listadepalavras.length);
+        for(var i = inicioparagrafo; i < fimparagrafo; i++){
+
+          if(i == 0){
+
+             textopronto += getRandomInt(0,listadepalavras.length).charAt(0).toUpperCase() + 
+             getRandomInt(1,listadepalavras.length).slice(1);
+
+          }else{
+
+             textopronto += " " + getRandomInt(1,listadepalavras.length);
+
+          }
 
         }
 
-     }
+        textopronto += ".";
 
-     textopronto += ".";
+        if(radiothtml){
+           textopronto += "<br>" + "&#60;&#47&#112&#62;";
+        }
+
+
      
      if(numeroparagrafos > 1){
-        textopronto += "<br><br>";
-     }
-     
+      textopronto += "<br><br>";
+    }
 
-   }
 
-   document.getElementById("textogerado").innerHTML = "" + textopronto;
+  }
+
+  document.getElementById("textogerado").innerHTML = "" + textopronto;
 
 });
